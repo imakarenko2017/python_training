f2feead84dcf246e7f1acb8904a2fb427fd54765
Inna Makarenko <inna.makarenko@gmail.com>
2017-08-03 13:30:37 +0300

    added recorded test

--- test_create_group.py
+++ test_create_group.py
@@ -1,12 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
-import time
-
-success = True
-wd = WebDriver()
-wd.implicitly_wait(60)
+import unittest
 
 def is_alert_present(wd):
     try:
@@ -15,33 +9,59 @@ def is_alert_present(wd):
     except:
         return False
 
-try:
-    wd.get("http://localhost/addressbook/")
-    wd.find_element_by_id("LoginForm").click()
-    wd.find_element_by_name("user").click()
-    wd.find_element_by_name("user").clear()
-    wd.find_element_by_name("user").send_keys("admin")
-    wd.find_element_by_name("pass").click()
-    wd.find_element_by_name("pass").clear()
-    wd.find_element_by_name("pass").send_keys("secret")
-    wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-    wd.find_element_by_link_text("groups").click()
-    wd.find_element_by_name("new").click()
-    wd.find_element_by_name("group_name").click()
-    wd.find_element_by_name("group_name").clear()
-    wd.find_element_by_name("group_name").send_keys("inna")
-    wd.find_element_by_name("group_header").click()
-    wd.find_element_by_name("group_header").clear()
-    wd.find_element_by_name("group_header").send_keys("inna")
-    wd.find_element_by_name("group_footer").click()
-    wd.find_element_by_name("group_footer").clear()
-    wd.find_element_by_name("group_footer").send_keys("inna")
-    wd.find_element_by_name("submit").click()
-    wd.find_element_by_link_text("group page").click()
-    wd.find_element_by_link_text("Logout").click()
-    wd.find_element_by_name("user").click()
-    wd.find_element_by_name("user").send_keys("\\9")
-finally:
-    wd.quit()
-    if not success:
-        raise Exception("Test failed.")
+class test_create_group(unittest.TestCase):
+    def setUp(self):
+        self.wd = WebDriver(capabilities={"marionette": False})
+        self.wd.implicitly_wait(60)
+    
+    def test_test_create_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin",password="secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, name= "new_group",header= "header_text",footer= "footer_text")
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_to_groups_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
+
+    def create_group(self, wd, name, header, footer):
+        wd.find_element_by_name("new").click()
+        # fill group form
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(footer)
+        # submitting form
+        wd.find_element_by_name("submit").click()
+
+    def open_groups_page(self, wd):
+        wd.find_element_by_link_text("groups").click()
+
+    def login(self, wd, username, password):
+        wd.find_element_by_id("header").click()
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
+
+    def tearDown(self):
+        self.wd.quit()
+
+if __name__ == '__main__':
+    unittest.main()
