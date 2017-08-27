d4cb7c7e6664c13802d2690026af06abd59ea2fd
Inna Makarenko <inna.makarenko@gmail.com>
2017-08-27 07:16:48 +0300

    добавлено предусловие для теста модификации групп

--- test/modify_group.py
+++ test/modify_group.py
@@ -3,13 +3,16 @@ from model.group import Group
 
 
 def test_modify_groupname(app):
+    if app.group.count()==0:
+        app.group.create(Group(name="Group_0814", header="hh"))
     app.group.modify_first_group(Group(name="Group_0814"))
 
 def test_modify_groupheader(app):
-    app.group.modify_first_group(Group(header="Header_0814"))
-    app.session.logout()
+    if app.group.count()==0:
+        app.group.create(Group(name="Group_0814", header="hh"))
+    app.group.modify_first_group(Group(header="hh"))
 
 def test_modify_groupfooter(app):
-    app.session.login(username="admin", password="secret")
+    if app.group.count()==0:
+        app.group.create(Group(name="Group_0814", footer="Footer 0814"))
     app.group.modify_first_group(Group(footer="Footer 0814"))
-    app.session.logout()
