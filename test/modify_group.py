d805e9860149cc7953dd171f0800867fb10cd7db
Inna Makarenko <inna.makarenko@gmail.com>
2017-09-03 15:54:54 +0300

    Исправлен вызов метода id_or_max

--- test/modify_group.py
+++ test/modify_group.py
@@ -4,24 +4,30 @@ from model.group import Group
 
 def test_modify_groupname(app):
     old_groups = app.group.get_group_list()
+    group=Group(name="Group_0814", header="hh")
+    group.id =old_groups[0].id
     if app.group.count()==0:
         app.group.create(Group(name="Group_0814", header="hh"))
-    app.group.modify_first_group(Group(name="Group_0814"))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups)  == len(new_groups)
-
-def test_modify_groupheader(app):
-    old_groups = app.group.get_group_list()
-    if app.group.count()==0:
-        app.group.create(Group(name="Group_0814", header="hh"))
-    app.group.modify_first_group(Group(header="hh"))
+    app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0]=group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-def test_modify_groupfooter(app):
-    old_groups = app.group.get_group_list()
-    if app.group.count()==0:
-        app.group.create(Group(name="Group_0814", footer="Footer 0814"))
-    app.group.modify_first_group(Group(footer="Footer 0814"))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
+
+
+#def test_modify_groupheader(app):
+#    old_groups = app.group.get_group_list()
+    #    if app.group.count()==0:
+    #    app.group.create(Group(name="Group_0814", header="hh"))
+    #    app.group.modify_first_group(Group(header="hh"))
+    #new_groups = app.group.get_group_list()
+#assert len(old_groups) == len(new_groups)
+
+#def test_modify_groupfooter(app):
+    #   old_groups = app.group.get_group_list()
+    #if app.group.count()==0:
+    #   app.group.create(Group(name="Group_0814", footer="Footer 0814"))
+    #app.group.modify_first_group(Group(footer="Footer 0814"))
+    #new_groups = app.group.get_group_list()
+#assert len(old_groups) == len(new_groups)
--- test/test_create_group.py
+++ test/test_create_group.py
@@ -9,7 +9,7 @@ def test_create_group(app):
     new_groups=app.group.get_group_list()
     assert len(old_groups)+1==len(new_groups)
     old_groups.append(group)
-    assert sorted(old_groups,key=group.id_or_max) == sorted(new_groups,key=group.id_or_max)
+    assert sorted(old_groups,key=Group.id_or_max) == sorted(new_groups,key=Group.id_or_max)
 
 
 
