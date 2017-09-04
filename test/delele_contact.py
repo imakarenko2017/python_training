4a6f826f037c93bd85b00ff8f243cdcf2487be46
Inna Makarenko <inna.makarenko@gmail.com>
2017-09-04 16:51:00 +0300

    Проверка длины списков контактов в тесте удаления контактов

--- test/delele_contact.py
+++ test/delele_contact.py
@@ -5,6 +5,10 @@ from model.contact import Contact
 def test_delete_first_contact (app):
     if app.contact.count() ==0:
         app.contact.create(Contact(firstname="Inessa",lastname="Molchanova"))
+    old_contacts = app.contact.get_contacts_list()
     app.contact.delete_first_contact()
+    app.contact.open_contacts_page()
+    new_contacts = app.contact.get_contacts_list()
+    assert len(new_contacts) == len(old_contacts) - 1
 
 
