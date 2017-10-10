94a2a815a100cbf3ca0c32738ba4ce0bc71d4884
Inna Makarenko <inna.makarenko@gmail.com>
2017-10-10 21:07:35 +0300

    Проверка в тесте удаление контакта добавлена

--- test/test_delele_contact.py
+++ test/test_delele_contact.py
@@ -11,6 +11,8 @@ def test_delete_some_contact (app):
     app.contact.open_contacts_page()
     new_contacts = app.contact.get_contacts_list()
     assert len(new_contacts) == len(old_contacts) - 1
+    del old_contacts[index]
+    assert sorted(old_contacts,key=Contact.id_or_max) == sorted(new_contacts,key=Contact.id_or_max)
 
 def test_delete_first_contact (app):
     if app.contact.count() ==0:
